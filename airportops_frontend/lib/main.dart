b0afc40fbaa7d5bfb43809e9253fb4f8fe3ac03e
Ethan Malzone <ejmalzone@gmail.com>
2023-02-02 13:39:43 -0500

    Passenger profile display line

--- airportops_frontend/lib/main.dart
+++ airportops_frontend/lib/main.dart
@@ -1,15 +1,29 @@
+import 'package:airportops_frontend/enums.dart';
 import 'package:airportops_frontend/passenger.dart';
 import 'package:flutter/material.dart';
+import 'package:airportops_frontend/extensions.dart';
 
 void main() {
-  runApp(const PassengerProfileApp());
+  runApp(PassengerProfileApp());
 }
 
 class PassengerProfileApp extends StatelessWidget {
-  const PassengerProfileApp({super.key});
+  PassengerProfileApp({super.key});
+
+  final Passenger testP = Passenger(
+      "Ethan",
+      "Malzone",
+      DateTime(2001, 1, 4),
+      "DTW",
+      DateTime(2023, 2, 2),
+      "IAH",
+      DateTime(2023, 2, 3),
+      "United States",
+      "21A");
 
   @override
   Widget build(BuildContext context) {
+    testP.status = Status.boarded;
     return MaterialApp(
       title: 'Passenger Profile',
       theme: ThemeData(
@@ -17,16 +31,7 @@ class PassengerProfileApp extends StatelessWidget {
       ),
       home: PassengerProfile(
           title: 'Passenger Profile',
-          passenger: Passenger(
-            "Ethan",
-            "Malzone",
-            DateTime(2001, 1, 4),
-            "DTW",
-            DateTime(2023, 2, 2),
-            "IAH",
-            DateTime(2023, 2, 3),
-            "United States",
-            "21A")),
+          passenger: testP)
     );
   }
 }
@@ -41,19 +46,49 @@ class PassengerProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(
+          title,
+          textAlign: TextAlign.center),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              passenger.fullName,
-              textAlign: TextAlign.left,
-              style: const TextStyle(fontSize: 32)
-            )
+            Card(
+              child:
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[Text(
+                            passenger.fullName,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(fontSize: 24)
+                        ),
+                          Text(
+                            '${passenger.birthday.toString().split(' ')[0]} | ${passenger.citizenship}',
+                            textAlign: TextAlign.justify,
+                            style: const TextStyle(fontSize: 12),
+                          )
+                        ]
+                      ),
+                      Text(
+                          passenger.status.name.titleCase(),
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 36,
+                              color: passenger.status.color
+                          )
+                      )
+                    ]
+                  )
+              )
+            ),
           ],
         ),
       ),
