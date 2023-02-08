f977aa62ad333838134d1d54c5c307e97e5b0031
austinleblanc <austin.c.leblanc@gmail.com>
2023-02-08 13:53:59 -0500

    Routes
    Implemented basic routes into main.dart

--- airportops_frontend/lib/main.dart
+++ airportops_frontend/lib/main.dart
@@ -4,8 +4,78 @@ import 'package:flutter/material.dart';
 import 'package:airportops_frontend/extensions.dart';
 import 'package:airportops_frontend/baggage.dart';
 
-void main() {
+/*void main() {
   runApp(PassengerProfileApp());
+}*/
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeRoute(),
+      '/passenger': (context) => PassengerProfileApp(),
+      '/baggage': (context) => const BaggageRoute(),
+      '/admin': (context) => const AdminRoute(),
+    },
+  ));
+}
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            child: const Text('Passenger Check-in'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/passenger');
+            },
+          ),
+          ElevatedButton(
+              child: const Text('Baggage Information'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/baggage');
+              }),
+          ElevatedButton(
+              child: const Text('Admin Panel'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/admin');
+              }),
+        ],
+      )),
+    );
+  }
+}
+
+class BaggageRoute extends StatelessWidget {
+  const BaggageRoute({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Baggage Page"),
+      ),
+    );
+  }
+}
+
+class AdminRoute extends StatelessWidget {
+  const AdminRoute({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Admin Page"),
+      ),
+    );
+  }
 }
 
 class PassengerProfileApp extends StatelessWidget {
@@ -25,14 +95,13 @@ class PassengerProfileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     testP.status = Status.boarded;
-    return MaterialApp(
-      title: 'Passenger Profile',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Passenger Profile'),
       ),
-      home: PassengerProfile(
-          title: 'Passenger Profile',
-          passenger: testP)
+      body: Center(
+          child:
+              PassengerProfile(title: 'Passenger Profile', passenger: testP)),
     );
   }
 }
@@ -47,11 +116,9 @@ class PassengerProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          title,
-          textAlign: TextAlign.center),
-      ),
+      /*appBar: AppBar(
+        title: Text(title, textAlign: TextAlign.center),
+      ),*/
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -59,38 +126,28 @@ class PassengerProfile extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
-              child:
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[Text(
-                            passenger.fullName,
-                            textAlign: TextAlign.left,
-                            style: const TextStyle(fontSize: 24)
-                        ),
-                          Text(
-                            '${passenger.birthday.toString().split(' ')[0]} | ${passenger.citizenship}',
-                            textAlign: TextAlign.justify,
-                            style: const TextStyle(fontSize: 12),
-                          )
-                        ]
-                      ),
-                      Text(
-                          passenger.status.name.titleCase(),
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              fontSize: 36,
-                              color: passenger.status.color
-                          )
-                      )
-                    ]
-                  )
-              )
-            ),
+                child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(children: <Widget>[
+                            Text(passenger.fullName,
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(fontSize: 24)),
+                            Text(
+                              '${passenger.birthday.toString().split(' ')[0]} | ${passenger.citizenship}',
+                              textAlign: TextAlign.justify,
+                              style: const TextStyle(fontSize: 12),
+                            )
+                          ]),
+                          Text(passenger.status.name.titleCase(),
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                  fontSize: 36, color: passenger.status.color))
+                        ]))),
           ],
         ),
       ),
