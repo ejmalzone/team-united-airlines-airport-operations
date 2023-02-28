32f37f2bed1fbc78284c15f8c0934a1eebda01d5
austinleblanc <austin.c.leblanc@gmail.com>
2023-02-27 19:53:01 -0500

    A new button appears when scanning a QR

--- airportops_frontend/lib/scanning.dart
+++ airportops_frontend/lib/scanning.dart
@@ -3,149 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:scan/scan.dart';
 import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
-
-// A screen that allows users to take a picture using a given camera.
-class TakePictureScreen extends StatefulWidget {
-  const TakePictureScreen({
-    super.key,
-    required this.camera,
-  });
-
-  final CameraDescription camera;
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<TakePictureScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  final _qrBarCodeScannerDialogPlugin = QrBarCodeScannerDialog();
-  String? code;
-
-  @override
-  void initState() {
-    super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Take a picture')),
-      // You must wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner until the
-      // controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
-
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
-
-            if (!mounted) return;
-
-            //ScanController controller = ScanController();
-            //String? barcodeResult = await Scan.parse(image.path);
-            //print(barcodeResult);
-
-            // If the picture was taken, display it on a new screen.
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image.path,
-                ),
-              ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
-    );
-  }
-}
-
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({super.key, required this.imagePath});
-
-  @override
-  Widget build(BuildContext context) {
-    if (kIsWeb) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Display the Picture')),
-        body: Column(
-          children: <Widget>[
-            Image.network(imagePath),
-            ElevatedButton(
-                onPressed: () async {
-                  String? result = await Scan.parse(imagePath);
-                  print('RESULT');
-                  print('Result: $result');
-                },
-                child: const Text('Process QR')),
-          ],
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Display the Picture')),
-        body: Center(
-            child: Column(
-          children: <Widget>[
-            Image.file(File(imagePath)),
-          ],
-        )),
-      );
-    }
-  }
-}
 
 class UniversalScanApp extends StatefulWidget {
   @override
@@ -158,12 +16,43 @@ class _UniversalScanAppState extends State<UniversalScanApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    if (kIsWeb) {
+      return Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('QR Scanning Page'),
         ),
         body: Builder(builder: (context) {
+          if (code != null) {
+            return Material(
+                child: Column(
+              children: <Widget>[
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+                          context: context,
+                          onCode: (code) {
+                            setState(() {
+                              this.code = code?.substring(15);
+                            });
+                          });
+                    },
+                    child: Text((() {
+                      if (code == null) {
+                        return 'Scan Tag';
+                      }
+                      return 'Scan Another Tag';
+                    }())),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Query based on ${this.code}'),
+                ),
+              ],
+            ));
+          }
+
           return Material(
             child: Center(
               child: ElevatedButton(
@@ -172,7 +61,7 @@ class _UniversalScanAppState extends State<UniversalScanApp> {
                         context: context,
                         onCode: (code) {
                           setState(() {
-                            this.code = code;
+                            this.code = code?.substring(15);
                           });
                         });
                   },
@@ -180,7 +69,29 @@ class _UniversalScanAppState extends State<UniversalScanApp> {
             ),
           );
         }),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Scanning Page'),
       ),
+      body: Builder(builder: (context) {
+        return Material(
+          child: Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+                      context: context,
+                      onCode: (code) {
+                        setState(() {
+                          this.code = code;
+                        });
+                      });
+                },
+                child: Text(code ?? "Scan Boarding Pass")),
+          ),
+        );
+      }),
     );
   }
 }
