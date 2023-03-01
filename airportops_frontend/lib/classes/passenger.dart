f465e9d22fe4f2954e5be54e1ae1880c9b18bf35
austinleblanc <austin.c.leblanc@gmail.com>
2023-02-28 21:40:01 -0500

    Added query for pulling passengers

--- airportops_frontend/lib/classes/passenger.dart
+++ airportops_frontend/lib/classes/passenger.dart
@@ -15,6 +15,28 @@ class Passenger {
   List<String> requests = [];
   Status status = Status.unboarded;
 
+  /* 
+  class Passenger {
+  final String passengerId;
+  final String nameFirst;
+  final String nameLast;
+  final DateTime birthday;
+  final int row;
+  final String seat;
+  bool boarded;
+  final String flightSource;
+  final String flightDestination;
+  final List<String> accomodations;
+  final String event;
+
+  //final int reservationNum;
+  //final DateTime flightSourceDate;
+  //final DateTime flightDestinationDate;
+  //final String citizenship;
+  //List<String> requests = [];
+  Status status = Status.unboarded;
+   */
+
   Passenger(
       {required this.nameFirst,
       required this.nameLast,
@@ -28,7 +50,26 @@ class Passenger {
       required this.seat,
       requests});
 
-  
+  /*
+      Passenger({
+    required this.passengerId,
+    required this.nameFirst,
+    required this.nameLast,
+    required this.birthday,
+    required this.row,
+    required this.seat,
+    required this.boarded,
+    required this.flightSource,
+    required this.flightDestination,
+    required this.accomodations,
+    required this.event,
+    //required this.reservationNum,
+    //required this.flightSourceDate,
+    //required this.flightDestinationDate,
+    //required this.citizenship,
+    //requests});
+  });
+       */
 
   bool isAdult() {
     return DateTime.now().difference(birthday).inDays > (365 * 18);
@@ -45,4 +86,14 @@ class Passenger {
       return requests.join(', ');
     }
   }
+
+  /*String get requestsString {
+    //if (requests.isEmpty) {
+    if (accomodations.isEmpty) {
+      return 'None';
+    } else {
+      //return requests.join(', ');
+      return accomodations.join(', ');
+    }
+  }*/
 }
