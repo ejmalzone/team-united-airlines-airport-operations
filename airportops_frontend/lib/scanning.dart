979e5efea61c3a27f874e8a8bf24c2650df6c030
austinleblanc <austin.c.leblanc@gmail.com>
2023-03-08 00:45:00 -0500

    Added honeywell scanner functionality

--- airportops_frontend/lib/scanning.dart
+++ airportops_frontend/lib/scanning.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:honeywell_scanner/honeywell_scanner.dart';
 import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
 import 'package:requests/requests.dart';
 
@@ -12,61 +13,77 @@ class HoneywellScanApp extends StatefulWidget {
 }
 
 class _HoneywellScanAppState extends State<HoneywellScanApp> {
-  final FocusNode _focusNode = FocusNode();
-  String? _message;
+  String? lastScan;
 
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  KeyEventResult _handleKeyEvent(FocusNode node, RawKeyEvent event) {
-    setState(() {
-      if (event.physicalKey == PhysicalKeyboardKey.keyA) {
-        _message = 'Pressed the key next to CAPS LOCK!';
-      } else {
-        if (kReleaseMode) {
-          _message =
-              'Not the key next to CAPS LOCK: Pressed 0x${event.physicalKey.usbHidUsage.toRadixString(16)}';
-        } else {
-          // As the name implies, the debugName will only print useful
-          // information in debug mode.
-          _message =
-              'Not the key next to CAPS LOCK: Pressed ${event.physicalKey.debugName}';
-        }
-      }
-    });
-    return event.physicalKey == PhysicalKeyboardKey.keyA
-        ? KeyEventResult.handled
-        : KeyEventResult.ignored;
-  }
+  late HoneywellScanner honeywellScanner;
+  bool supported = false;
 
   @override
   Widget build(BuildContext context) {
+    honeywellScanner = HoneywellScanner(
+      onScannerDecodeCallback: (scannedData) {
+        setState(() {
+          lastScan = scannedData?.code;
+        });
+        showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Scan Results'),
+                  content:
+                      const Text('Do you want to check in the scanned bag?'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Cancel'),
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                    ),
+                    TextButton(
+                      child: Text('Check In id no. ${scannedData?.code}'),
+                      onPressed: () async {
+                        if (scannedData != null) {}
+                        await Requests.put(
+                            'http://ec2-52-3-243-69.compute-1.amazonaws.com:5000/api/passenger/',
+                            json: {"passengerId": scannedData?.code});
+                        Navigator.of(context).pop('OK');
+                      },
+                    )
+                  ],
+                ));
+      },
+      onScannerErrorCallback: (error) {},
+    );
+    honeywellScanner.startScanner();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Honeywell Scanner Testing'),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: Text('Test device support: $supported'),
+              onTap: () async {
+                bool supp = await honeywellScanner.isSupported();
+                setState(() {
+                  supported = supp;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
       body: Container(
           alignment: Alignment.center,
-          child: Focus(
-            focusNode: _focusNode,
-            onKey: _handleKeyEvent,
-            child: AnimatedBuilder(
-                animation: _focusNode,
-                builder: (BuildContext context, Widget? child) {
-                  if (!_focusNode.hasFocus) {
-                    return GestureDetector(
-                      onTap: () {
-                        FocusScope.of(context).requestFocus(_focusNode);
-                      },
-                      child: const Text('Click to focus'),
-                    );
-                  }
-                  return Text(_message ?? 'Press a key');
-                }),
-          )),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text('Last scan result: '),
+            Text(lastScan ?? 'No Recent Scans'),
+          ])),
     );
   }
 }
