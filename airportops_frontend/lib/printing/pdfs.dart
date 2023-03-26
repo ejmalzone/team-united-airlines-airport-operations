5345531f424a5b39579596a7ee7e4096c81d76dd
austinleblanc <austin.c.leblanc@gmail.com>
2023-03-26 17:47:43 -0400

    Commented out code stopping android compilation

--- airportops_frontend/lib/printing/pdfs.dart
+++ airportops_frontend/lib/printing/pdfs.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:html' as html;
 import 'dart:io' as io;
 
 import 'package:flutter/cupertino.dart';
@@ -8,7 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-
+//import 'dart:html' as html; TODO: Figure out how to make android compile with this being imported still?
 import 'package:airportops_frontend/classes/baggage.dart';
 
 import '../classes/passenger.dart';
@@ -37,7 +36,7 @@ class PdfCreator {
 
   static generateBagPdf(final Baggage bag) async {
     final bytes = await rootBundle.load('assets/united-high-res.png');
-    final _unitedLogo  = pw.MemoryImage(bytes.buffer.asUint8List());
+    final _unitedLogo = pw.MemoryImage(bytes.buffer.asUint8List());
 
     final pdf = pw.Document();
 
@@ -48,48 +47,41 @@ class PdfCreator {
         pageFormat: PdfPageFormat.roll80,
         build: (pw.Context context) {
           return pw.Center(
-            child: pw.Column(
-              children: [
-                pw.BarcodeWidget(
-                  data: code,
-                  barcode: pw.Barcode.qrCode(),
-                  width: 200,
-                  height: 200
-                ),
-                pw.SizedBox(height: 20),
-                pw.Image(_unitedLogo),
-                pw.SizedBox(height: 20),
-                pw.BarcodeWidget(
-                    data: code,
-                    barcode: pw.Barcode.qrCode(),
-                    width: 200,
-                    height: 200
-                ),
-                pw.SizedBox(height: 20),
-                pw.BarcodeWidget(
-                    data: code,
-                    barcode: pw.Barcode.qrCode(),
-                    width: 200,
-                    height: 200
-                ),
-                pw.SizedBox(height: 10),
-                pw.Text(DateTime.now().toIso8601String())
-              ]
-            )
-          );// Center
-        }
-      )
-    ); // Page
+              child: pw.Column(children: [
+            pw.BarcodeWidget(
+                data: code,
+                barcode: pw.Barcode.qrCode(),
+                width: 200,
+                height: 200),
+            pw.SizedBox(height: 20),
+            pw.Image(_unitedLogo),
+            pw.SizedBox(height: 20),
+            pw.BarcodeWidget(
+                data: code,
+                barcode: pw.Barcode.qrCode(),
+                width: 200,
+                height: 200),
+            pw.SizedBox(height: 20),
+            pw.BarcodeWidget(
+                data: code,
+                barcode: pw.Barcode.qrCode(),
+                width: 200,
+                height: 200),
+            pw.SizedBox(height: 10),
+            pw.Text(DateTime.now().toIso8601String())
+          ])); // Center
+        })); // Page
 
     final name = 'baggage_tag_${_id++}.pdf';
     final Uint8List pdfInBytes = await pdf.save();
 
     if (kIsWeb) {
       final content = base64Encode(pdfInBytes);
-      final anchor = html.AnchorElement(
-          href: "data:application/octet-stream;charset=utf-16le;base64,$content")
+      /*final anchor = html.AnchorElement(
+          href:
+              "data:application/octet-stream;charset=utf-16le;base64,$content")
         ..setAttribute("download", name)
-        ..click();
+        ..click();*/
     } else {
       final file = io.File('${await _localPath}/$name');
       await file.writeAsBytes(pdfInBytes);
