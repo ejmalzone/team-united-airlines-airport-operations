861fe6bfd250d9da4d2e867d83acc3a7a12ebad2
austinleblanc <austin.c.leblanc@gmail.com>
2023-04-10 20:11:38 -0400

    Updates for mobile friendliness

--- airportops_frontend/lib/admin/toggles/csr.dart
+++ airportops_frontend/lib/admin/toggles/csr.dart
@@ -90,14 +90,14 @@ class _AdminPassengersState extends State<AdminPassengers> {
         ),
         Container(
           width: 100,
-          height: 100,
+          //height: 100,
           child: Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
                 width: 100,
-                height: 100,
+                //height: 100,
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
                   children: [
@@ -121,7 +121,7 @@ class _AdminPassengersState extends State<AdminPassengers> {
                 ),
               ),
               SizedBox(
-                height: 100,
+                height: 50,
                 child: VerticalDivider(
                   thickness: 1,
                   color: Colors.black,
@@ -129,7 +129,7 @@ class _AdminPassengersState extends State<AdminPassengers> {
               ),
               Container(
                 width: 100,
-                height: 100,
+                //height: 100,
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
                   children: [
@@ -153,7 +153,7 @@ class _AdminPassengersState extends State<AdminPassengers> {
                 ),
               ),
               SizedBox(
-                height: 100,
+                height: 50,
                 child: VerticalDivider(
                   thickness: 1,
                   color: Colors.black,
@@ -161,7 +161,7 @@ class _AdminPassengersState extends State<AdminPassengers> {
               ),
               Container(
                 width: 100,
-                height: 100,
+                //height: 100,
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
                   children: [
